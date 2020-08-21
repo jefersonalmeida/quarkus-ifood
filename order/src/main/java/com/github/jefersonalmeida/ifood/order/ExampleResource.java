eb2480af02e20276ccae0fb8e13e3085e83edf33
Jeferson Almeida <jeferson.almeida@outlook.com>
2020-08-21 10:36:22 -0400

    centralizando logs

--- order/src/main/java/com/github/jefersonalmeida/ifood/order/ExampleResource.java
+++ order/src/main/java/com/github/jefersonalmeida/ifood/order/ExampleResource.java
@@ -1,16 +1,23 @@
 package com.github.jefersonalmeida.ifood.order;
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.time.LocalDateTime;
 
 @Path("/hello")
 public class ExampleResource {
 
+    private static final Logger LOG = Logger.getLogger(ExampleResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("Olá Quarkus");
+        LOG.infov("Olá Quarkus {0},", LocalDateTime.now());
         return "hello";
     }
 }
